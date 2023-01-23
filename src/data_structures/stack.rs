d93d4426759b7a20aa47500b8268fd52f27bdb0a
Caleb Freitas <calebfreitas@tutanota.com>
2023-01-23 19:12:50 -0300

    feat: implemented a simpler version of stack data structure

--- src/data_structures/stack.rs
+++ src/data_structures/stack.rs
@@ -1,92 +1,108 @@
-use std::ptr;
+use core::fmt::Debug;
 
+#[derive(Debug)]
 pub struct Stack<T> {
-    // stores the current number of elements in the stack
-    size: usize,
-
-    // stores the offset of the top element in the stack
-    offset: usize,
-
-    // stores the maximum number of elements that the stack can hold
-    capacity: usize,
-
-    // raw pointer to the memory location where the elements of the stack are stored
-    data: *mut T,
+    top: usize,
+    data: Vec<Option<T>>,
 }
 
-impl<T> Stack<T> {
-    // initializes the stack by setting the pointer (data) to null, capacity, offset and
-    // size to zero, and allocating memory for the stack using `libc` malloc function
-    pub unsafe fn new(&mut self, capacity: usize) {
-        // creates a null mutable raw pointer
-        self.data = ptr::null_mut();
-        self.size = 0;
-        self.offset = 0;
-        self.capacity = 0;
-
-        // allocate memory for the stack
-        if capacity > 0 {
-            self.data = libc::malloc(capacity * std::mem::size_of::<T>()) as *mut T;
-            self.capacity = capacity;
+impl<T: Clone + Debug> Stack<T> {
+    pub fn new() -> Self {
+        Self {
+            top: 0,
+            data: vec![None; 5],
         }
     }
 
-    // increase the capacity of the stack when the stack is full. it uses the `libc`
-    // malloc function to allocate new memory, copies the elements from the old
-    // memory location to a new one, and frees the old memory using libc's free function
-    pub unsafe fn grow(&mut self, new_capacity: usize) {
-        // allocate memory for the new data with new capacity
-        let new_data = libc::malloc(new_capacity * std::mem::size_of::<T>()) as *mut T;
-        let old_size = self.size;
-
-        // copies the elements from the old memory location to the new one (new_data)
-        ptr::copy_nonoverlapping(self.data, new_data, old_size);
-
-        // frees the old memory
-        libc::free(self.data as *mut libc::c_void);
-
-        self.data = new_data;
-        self.capacity = new_capacity;
-    }
-
-    // insert an element at the top of the stack
-    pub unsafe fn insert(&mut self, value: T) {
-        // checks if the stack is full, if it is, increase the capacity of the stack
-        if self.size == self.capacity {
-            self.grow(self.capacity + 2);
+    pub fn push(&mut self, item: T) {
+        if self.is_full() {
+            self.increase_array_size();
         }
 
-        // overwrites a memory location with the given value
-        ptr::write(self.data.offset(self.offset as isize), value);
-
-        // define new values for `size` and `offset`
-        self.size += 1; 
-        self.offset = (self.offset + 1) % self.capacity;
+        self.data[self.top] = Some(item);
+        self.top += 1;
     }
 
-    // remove the top element from the stack
-    pub unsafe fn delete(&mut self) -> Option<T> {
-        if self.size == 0 {
+    pub fn pop(&mut self) -> Option<T> {
+        if self.is_empty() {
             return None;
         }
 
-        self.size -= 1;
-        self.offset = (self.offset + self.capacity - 1) % self.capacity;
+        let item = self.data[self.top - 1].clone();
+        self.top -= 1;
 
-        Some(ptr::read(self.data.offset(self.offset as isize)))
+        return item;
     }
 
-    // properly deallocate the memory used by the stack
-    pub unsafe fn drop(&mut self) {
-        while self.size > 0 {
-            self.size -= 1;
-            self.offset = (self.offset + self.capacity - 1) % self.capacity;
+    fn increase_array_size(&mut self) {
+        let current_capacity = self.data.len();
+        let new_capacity = current_capacity + 5;
+        let mut new_data = vec![None; new_capacity];
 
-            // executes the destructor (if any) of the pointed-to value for every
-            // element in the stack
-            ptr::drop_in_place(self.data.offset(self.offset as isize));
+        for i in 0..current_capacity {
+            new_data[i] = self.data[i].clone();
         }
-        // frees the memory allocated for the stack
-        libc::free(self.data as *mut libc::c_void);
+
+        self.data = new_data;
+    }
+    
+    fn is_full(&self) -> bool {
+        return self.top == self.data.len();
+    }
+
+    fn is_empty(&self) -> bool {
+        return self.top <= 0;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create() {
+        let stack: Stack<i32> = Stack::new();
+        assert_eq!(stack.top, 0);
+        assert_eq!(stack.data.len(), 5);
+    }
+
+    #[test]
+    fn test_push() {
+        let mut stack: Stack<i32> = Stack::new();
+        
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+
+        assert_eq!(stack.top, 6);
+        assert_eq!(stack.data.len(), 10);
+    }
+
+    #[test]
+    fn test_pop() {
+        let mut stack: Stack<i32> = Stack::new();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        assert_eq!(stack.pop(), Some(3));
+        assert_eq!(stack.pop(), Some(2));
+        assert_eq!(stack.pop(), Some(1));
+        assert_eq!(stack.pop(), None);
+    }
+
+    #[test]
+    fn test_is_empty() {
+        let mut stack: Stack<i32> = Stack::new();
+
+        assert!(stack.is_empty());
+
+        stack.push(1);
+
+        assert!(!stack.is_empty());
     }
 }
