66bd8020eee38fc090af3f7c2d63655e60cb61be
Caleb Freitas <calebfreitas@tutanota.com>
2023-01-23 20:08:05 -0300

    test: added test for empty array

--- src/searching/linear_search.rs
+++ src/searching/linear_search.rs
@@ -1,10 +1,15 @@
-pub fn linear_search<T: PartialEq>(vec: &[T], target: T) -> Option<usize> {
-    for (item_index, item) in vec.iter().enumerate() {
+pub fn linear_search<T: PartialEq>(arr: &[T], target: T) -> Option<usize> {
+
+    if arr.len() == 0 {
+        return None;
+    }
+
+    for (index, item) in arr.iter().enumerate() {
         if *item == target {
-            return Some(item_index);
+            return Some(index);
         }
     }
-    None
+    return None;
 }
 
 #[cfg(test)]
@@ -12,7 +17,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_linear_search_target_exist() {
+    fn test_target_exist() {
         let arr = [1, 2, 3, 4, 5];
         let target = 3;
         let index = linear_search(&arr, target);
@@ -20,11 +25,19 @@ mod tests {
     }
 
     #[test]
-    fn test_linear_search_target_do_not_exist() {
+    fn test_target_dont_exist() {
         let arr = [1, 2, 3, 4, 5];
         let target = 9;
         let index = linear_search(&arr, target);
         assert_eq!(index, None);
     }
-}
 
+    #[test]
+    fn test_edge_case_empty() {
+        let arr: Vec<i32> = vec![];
+        let target = 0;
+        let expected = None;
+        let res = linear_search(&arr, target);
+        assert_eq!(res, expected);
+    }
+}
