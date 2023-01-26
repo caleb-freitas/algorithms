c07ecb7961ded2bf7bb502dd14917cfe8d7fda63
Caleb Freitas <calebfreitas@tutanota.com>
2023-01-26 20:45:28 -0300

    test: added tests for insertion sort

--- src/sorting/insertion_sort.rs
+++ src/sorting/insertion_sort.rs
@@ -1,4 +1,4 @@
-pub fn insertion_sort(a: &mut [i32]) {
+pub fn insertion_sort(a: &mut [i32]) -> &mut [i32]{
     for current in 1..a.len() {
         // assume that key is the current element
         let key = a[current];
@@ -12,5 +12,59 @@ pub fn insertion_sort(a: &mut [i32]) {
             previous -= 1;
         }
         a[previous + 1] = key;
+    }
+    return a;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_already_sorted() {
+        let mut arr = vec![1, 2, 3, 4, 5];
+        let expected = vec![1, 2, 3, 4, 5];
+        let res = insertion_sort(&mut arr);
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn test_reverse_sorted() {
+        let mut arr = vec![5, 4, 3, 2, 1];
+        let expected = vec![1, 2, 3, 4, 5];
+        let res = insertion_sort(&mut arr);
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn test_random_sorted() {
+        let mut arr = vec![3, 5, 1, 4, 2];
+        let expected = vec![1, 2, 3, 4, 5];
+        let res = insertion_sort(&mut arr);
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn test_duplicate_elements() {
+        let mut arr = vec![3, 5, 1, 5, 4, 2];
+        let expected = vec![1, 2, 3, 4, 5, 5];
+        let res = insertion_sort(&mut arr);
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn test_edge_case_empty() {
+        let mut arr: Vec<i32> = vec![];
+        let expected = vec![];
+        let res = insertion_sort(&mut arr);
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn test_edge_case_single_element() {
+        let mut arr = vec![1];
+        let expected = vec![1];
+        let res = insertion_sort(&mut arr);
+        assert_eq!(res, expected);
     }
 }
